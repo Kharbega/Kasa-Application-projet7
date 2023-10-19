fac0b6848cd6799912a77719b47c73d218dc0b34
Kharbega Soumia <109501749+Kharbega@users.noreply.github.com>
2023-10-19 22:35:02 +0200

    page error en cours

--- src/pages/Error.js
+++ src/pages/Error.js
@@ -1,10 +1,23 @@
 import React from 'react';
+import { NavLink } from 'react-router-dom';
+import Logo from '../components/Logo';
+
 
 const Error = () => {
     return (
         <div className='ErrorPage'>
+            <Logo />
             <div className="Errorpage-content">
-                <h2>Erreur 404</h2>
+                <div className="Text-404-Content">
+                    <h2 className='Text-404'>404</h2>
+                </div>
+                <div className="Text-H3-Content">
+
+                    <h3 className='Text-H3'>Oups! La page que vous demandez n'existe pas.</h3>
+                </div>
+                <NavLink to="/" >
+                    <h4>Retourner sur la page d'accueil</h4>
+                </NavLink>
             </div>
         </div>
     );
