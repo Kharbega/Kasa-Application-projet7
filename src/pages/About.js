98cad1f34e8959794385f3f857fd6ca07f2d1866
Kharbega Soumia <109501749+Kharbega@users.noreply.github.com>
2023-11-07 13:59:27 +0100

    apparition du chevron ,gagne !

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faChevronUp } from '@fortawesome/free-solid-svg-icons';
-const elementChevron = <FontAwesomeIcon icon="fa-solid fa-chevron-up" />
+const elementChevronUp = <FontAwesomeIcon icon={faChevronUp} />
 
 
 const About = () => {
@@ -11,25 +11,7 @@ const About = () => {
             <div className="logement-description-about">
                 <div className='title-chevron' >
                     <h4>Description</h4>
-                    <img src='src/assets/fonts/img/chevron-down-solid.svg'></img>
-                </div>
-                <p>Vous serez à 50m du canal Saint-martin où vous pourrez pique-niquer l'été et à côté de nombreux bars et restaurants. Au cœur de Paris avec 5 lignes de métro et de nombreux bus. Logement parfait pour les voyageurs en solo et les voyageurs d'affaires. Vous êtes à1 station de la gare de l'est (7 minutes à pied). </p>
-            </div>
-
-            <div className="logement-description-about">
-                <div className='title-chevron' >
-                    <h4>Description</h4>
-                    <img src='src/assets/fonts/img/chevron-down-solid.svg'></img>
-                </div>
-                <p>Vous serez à 50m du canal Saint-martin où vous pourrez pique-niquer l'été et à côté de nombreux bars et restaurants. Au cœur de Paris avec 5 lignes de métro et de nombreux bus. Logement parfait pour les voyageurs en solo et les voyageurs d'affaires. Vous êtes à1 station de la gare de l'est (7 minutes à pied). </p>
-            </div>
-
-
-            <div className="logement-description-about">
-                <div className='title-chevron' >
-
-                    <h4>Description</h4>
-                    <img src='src/assets/fonts/img/chevron-down-solid.svg'></img>
+                    <span className='elementChevronUp'>{elementChevronUp}</span>
 
                 </div>
                 <p>Vous serez à 50m du canal Saint-martin où vous pourrez pique-niquer l'été et à côté de nombreux bars et restaurants. Au cœur de Paris avec 5 lignes de métro et de nombreux bus. Logement parfait pour les voyageurs en solo et les voyageurs d'affaires. Vous êtes à1 station de la gare de l'est (7 minutes à pied). </p>
@@ -37,11 +19,32 @@ const About = () => {
 
             <div className="logement-description-about">
                 <div className='title-chevron' >
+                    <h4>Description</h4>
+                    <span className='elementChevronUp'>{elementChevronUp}</span>
+
+                </div>
+                <p>Vous serez à 50m du canal Saint-martin où vous pourrez pique-niquer l'été et à côté de nombreux bars et restaurants. Au cœur de Paris avec 5 lignes de métro et de nombreux bus. Logement parfait pour les voyageurs en solo et les voyageurs d'affaires. Vous êtes à1 station de la gare de l'est (7 minutes à pied). </p>
+            </div>
+
+
+            <div className="logement-description-about">
+                <div className='title-chevron' >
+
+                    <h4>Description</h4>
+                    <span className='elementChevronUp'>{elementChevronUp}</span>
+
+
+                </div>
+                <p>Vous serez à 50m du canal Saint-martin où vous pourrez pique-niquer l'été et à côté de nombreux bars et restaurants. Au cœur de Paris avec 5 lignes de métro et de nombreux bus. Logement parfait pour les voyageurs en solo et les voyageurs d'affaires. Vous êtes à1 station de la gare de l'est (7 minutes à pied). </p>
+            </div>
+
+            <div className="logement-description-about">
+                <div className='title-chevron' >
 
 
 
                     <h4>Description</h4>
-                    <span>{chevron}</span>
+                    <span className='elementChevronUp'>{elementChevronUp}</span>
                 </div>
 
                 <p>Vous serez à 50m du canal Saint-martin où vous pourrez pique-niquer l'été et à côté de nombreux bars et restaurants. Au cœur de Paris avec 5 lignes de métro et de nombreux bus. Logement parfait pour les voyageurs en solo et les voyageurs d'affaires. Vous êtes à1 station de la gare de l'est (7 minutes à pied). </p>
