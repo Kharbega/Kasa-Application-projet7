fd7e4e78d9241e29870fc1a20fde3fd77929e991
Kharbega Soumia <109501749+Kharbega@users.noreply.github.com>
2023-11-10 09:33:39 +0100

    collapse checked

--- src/components/LogementPanel.js
+++ src/components/LogementPanel.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faChevronUp, faChevronDown } from '@fortawesome/free-solid-svg-icons';
+import { faChevronUp } from '@fortawesome/free-solid-svg-icons';
+import { faChevronDown } from '@fortawesome/free-solid-svg-icons';
 
 const elementChevronUp = <FontAwesomeIcon icon={faChevronUp} />
 const elementChevronDown = <FontAwesomeIcon icon={faChevronDown} />
@@ -9,33 +10,29 @@ import "../styles/components/LogementPanel.css"
 
 const LogementPanel = (props) => {
 
-    function HandleClick() {
-        const [CollapseOpen, SetCollapseOpen] = useState(false)
 
-        /*Si* le collapse est ouvert notre <p> est visible sinon il est hidden */
-        const CollapseVisible = (CollapseOpen ? "visible " : "hidden") + HandleClick
+    const [CollapseOpen, SetCollapseOpen] = useState(false)
 
-        /* Si le <p> est visible alors on affiche le chevron Up sinon on affiche le chevronDown */
-        const CollapseChevron = (CollapseVisible ? elementChevronUp : elementChevronDown)
+    /* Si le <p> est visible alors on affiche le chevron Up sinon on affiche le chevronDown */
+    const CollapseChevron = (CollapseOpen ? elementChevronUp : elementChevronDown)
+    const ReverseContentTitle = () => { SetCollapseOpen(!CollapseOpen) }
 
-        /*  */
-        const ReverseContentTitle = () => { CollapseOpen(!SetCollapseOpen) }
+    /*  */
 
-        HandleClick()
 
-        return (
-            <div className="logement-panel-about">
-                <div className='group-title-chevron' onClick={ReverseContentTitle}>
-                    <h4>{props.title}</h4>
-                    <span className='elementChevronUp'>{CollapseChevron}</span>
-                </div>
-                <p className={CollapseOpen}>{props.content} </p>
+
+    return (
+        <div className="logement-panel-about">
+            <div className='group-title-chevron' onClick={ReverseContentTitle}>
+                <h4>{props.title}</h4>
+                <span className="elementChevronUp">{CollapseChevron}</span>
             </div>
+            {CollapseOpen && <p >{props.content} </p>}
+        </div>
 
-        );
-    };
-}
+    );
 
+};
 
 
 export default LogementPanel;
